64757144448b6a65f1171063e31d3bb94eb84ee9
mrkaspa <michel.ingesoft@gmail.com>
2016-12-09 17:40:49 -0500

    v0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GenAMQP.Mixfile do
 
   def project do
     [app: :gen_amqp,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      description: description,
      package: package,
