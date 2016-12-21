dca739d85d98e1181a6b3eed413866b0184a0fe1
mrkaspa <michel.ingesoft@gmail.com>
2016-12-21 11:42:58 -0500

    added timeout

--- lib/gen_amqp/client.ex
+++ lib/gen_amqp/client.ex
@@ -6,25 +6,29 @@ defmodule GenAMQP.Client do
   alias GenAMQP.Conn
 
   @spec call(String.t, String.t) :: any
- def call(exchange, payload) do
-   {:ok, pid} = Supervisor.start_child(GenAMQP.Supervisor, [])
-   {:ok, correlation_id} = Conn.request(pid, exchange, payload)
-   wait_response(pid, correlation_id)
- end
+  def call(exchange, payload) do
+    {:ok, pid} = Supervisor.start_child(GenAMQP.Supervisor, [])
+    {:ok, correlation_id} = Conn.request(pid, exchange, payload)
+    wait_response(pid, correlation_id)
+  end
 
- @spec publish(String.t, String.t) :: any
- def publish(exchange, payload) do
-   {:ok, pid} = Supervisor.start_child(GenAMQP.Supervisor, [])
-   Conn.publish(pid, exchange, payload)
-   :ok = Supervisor.terminate_child(GenAMQP.Supervisor, pid)
- end
+  @spec publish(String.t, String.t) :: any
+  def publish(exchange, payload) do
+    {:ok, pid} = Supervisor.start_child(GenAMQP.Supervisor, [])
+    Conn.publish(pid, exchange, payload)
+    :ok = Supervisor.terminate_child(GenAMQP.Supervisor, pid)
+  end
 
- def wait_response(pid, correlation_id) do
-   receive do
-     {:basic_deliver, payload, %{correlation_id: ^correlation_id}} ->
-       :ok = Supervisor.terminate_child(GenAMQP.Supervisor, pid)
-       payload
-     _ -> wait_response(pid, correlation_id)
-   end
+  def wait_response(pid, correlation_id) do
+    receive do
+      {:basic_deliver, payload, %{correlation_id: ^correlation_id}} ->
+        :ok = Supervisor.terminate_child(GenAMQP.Supervisor, pid)
+        payload
+      _ ->
+        wait_response(pid, correlation_id)
+    after
+      5_000 ->
+        {:error, :timeout}
+    end
   end
 end
