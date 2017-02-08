9c410e6c083db56a5a374f95c8b6842fc6940eec
mrkaspa <michel.ingesoft@gmail.com>
2017-02-08 12:54:49 -0500

    app config

--- lib/gen_amqp.ex
+++ lib/gen_amqp.ex
@@ -6,9 +6,15 @@ defmodule GenAMQP do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    conn_name = Application.get_env(:gen_amqp, :conn_name)
+    static_sup_name = Application.get_env(:gen_amqp, :static_sup_name)
+    dynamic_sup_name = Application.get_env(:gen_amqp, :dynamic_sup_name)
+
+    sup_name = static_sup_name || dynamic_sup_name
+
     # Define supervisors and child supervisors to be supervised
     children = [
-      supervisor(GenAMQP.ConnSupervisor, []),
+      supervisor(GenAMQP.ConnSupervisor, [sup_name, conn_name]),
     ]
 
     opts = [strategy: :one_for_one, name: GenAMQP.AppSupervisor]
